d54cb89a13e96af7f678545fa6d1f64c501f9d1d
Syba789 <95074177+Syba789@users.noreply.github.com>
2022-02-16 20:09:40 +0000

    done

--- p5final.c
+++ p5final.c
@@ -1,12 +1,33 @@
 #include<stdio.h>
+#include<math.h>
 float input()
 {
   float n;
-  printf("square root of a number\n");
+  printf("enter the number to find the square\n");
   scanf("%f",&n);
   return n;
 }
 void output(float n,float sqrt_result)
 {
-  printf("square root of %d is %d",n,sqrt r)
+  printf("%f is the square of %f",n,sqrt_result);
+}
+float mysqrt(float n)
+{
+  float guess=n/2;
+  float next;
+  next=n/guess;
+  while(fabs(next - guess)> 0.00001)
+  {
+    guess=(guess + next)/2;
+    next= n/guess;
+  }
+  return next;
+}
+int main()
+{
+  float n,sqrt;
+  n=input();
+  sqrt=mysqrt(n);
+  output(n,sqrt);
+  return 0;
 }
